f851419f9b3c63f0601b824b4912401d45e669cb
AATC Developer <dev@aatc.local>
2026-09-20 22:07:36 +0800

    Security: remove scratch test scripts from repo

--- scratch_test_env.py
+++
@@ -1,6 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-print("Before dotenv:", os.environ.get("GROQ_API_KEY"))
-load_dotenv()
-print("After dotenv:", os.environ.get("GROQ_API_KEY"))
--- scratch_test_keys.py
+++
@@ -1,3 +0,0 @@
-import os
-print("OPENAI_API_KEY:", os.environ.get("OPENAI_API_KEY"))
-print("GROQ_API_KEY:", os.environ.get("GROQ_API_KEY"))
